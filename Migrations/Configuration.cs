02457e50e35922d12fb47350e67c35978deb9f9d
Annemsucharitha <sucharithaannem@gmail.com>
2024-03-01 00:50:50 +0530

    finals

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿namespace DALLBlog.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DALLBlog.BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DALLBlog.BlogDbContext context)
+        {
+            context.AdminInfo.Add(new AdminInfo { EmailId = "suchi@admin.com", Password = "1234" });
+            context.SaveChanges();
+            base.Seed(context);
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
